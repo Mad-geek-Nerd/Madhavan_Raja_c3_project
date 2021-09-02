5869761aea6a35418cc4829db27b98be97192f06
Mad-geek-Nerd <madhavanraja5@gmail.com>
2021-09-02 21:54:10 +0530

    first commit

---
+++ Restaurant-finder/src/main/java/RestaurantService.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName){
+        Restaurant restaurant = findRestaurantByName(restaurantName);
+        restaurants.get(restaurant);
+        return  restaurant;
+    }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+    public int totalOrderCost(List<String> itemNames){
+        Restaurant totalOrderCost = List<Item>();
+
+
+    }
+}
